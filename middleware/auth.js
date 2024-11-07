6fddf2fbc298b0b7015d4474915878d95e269fae
JIshanRandika <jishanrandika@gmail.com>
2024-11-07 19:09:56 +0530

    Add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+import { verify } from 'jsonwebtoken';
+
+const auth = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
+export default auth;
