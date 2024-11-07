3eb01faaa2fc3baa2d6d6f28a115f2bdf4681e07
JIshanRandika <jishanrandika@gmail.com>
2024-11-07 19:11:00 +0530

    Update server

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-import express, { json } from "express";
-import { connect } from "mongoose";
-import cors from "cors";
-import { config } from "dotenv";
-import authRoutes from "./routes/auth.js"; // Use import for authRoutes
-import driverRoutes from "./routes/drivers.js"; // Use import for driverRoutes
+require('dotenv').config();
+import express, { json } from 'express';
+import connectDB from './config/db';
+import userRoutes from './routes/userRoutes';
+import driverRoutes from './routes/driverRoutes';
 
-config();
 const app = express();
-app.use(cors());
+connectDB();
 app.use(json());
 
-connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/drivers", driverRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/drivers', driverRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
