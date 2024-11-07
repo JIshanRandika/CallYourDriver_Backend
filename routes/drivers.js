2413525245a6af3abbdf727a4d87107cd4477f86
JIshanRandika <jishanrandika@gmail.com>
2024-11-07 19:09:41 +0530

    Delete drivers routes

--- routes/drivers.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express"; // Use import instead of require
-import { addDriver, suggestDriver } from "../controllers/driverController.js"; // Use import for controllers
-import authMiddleware from "../middlewares/authMiddleware.js";
-const router = Router();
-
-router.post("/add", authMiddleware, addDriver);
-router.get("/suggest", authMiddleware, suggestDriver);
-
-export default router; // Ensure this is present
