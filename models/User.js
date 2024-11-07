8b4b0a5ac343284d6c0f8dbd234bedcd8572882b
JIshanRandika <jishanrandika@gmail.com>
2024-11-07 19:12:26 +0530

    Update user modles

--- models/User.js
+++ models/User.js
@@ -1,18 +1,6 @@
-import { Schema, model } from "mongoose"; // Use ES module imports
-import hash from "bcryptjs"; // Import the entire bcryptjs module
-
-const User = new Schema({
-  name: { type: String, required: true },
-  contactNumber: { type: String, required: true },
+import { Schema, model } from 'mongoose';
+const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }
 });
-
-User.pre("save", async function(next) {
-  if (this.isModified("password")) {
-    this.password = await hash(this.password, 10);
-  }
-  next();
-});
-
-export default User; // This is the default export
+export default model('User', userSchema);
