b574406a66ff77419b9be5b4177caf72ae943a6f
JIshanRandika <jishanrandika@gmail.com>
2024-11-07 19:08:32 +0530

    Delete auth routes

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-import {Router} from "express"; // Use import instead of require
-import { register, login } from "../controllers/authController.js"; // Importing functions with ES module syntax
-const router = Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-export default router; // Ensure this is present
