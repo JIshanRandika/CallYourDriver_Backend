258118eb500e84aab7d6123ee965ffff0988142e
JIshanRandika <jishanrandika@gmail.com>
2024-11-09 08:46:07 +0530

    Update driver model

--- models/Driver.js
+++ models/Driver.js
@@ -3,7 +3,14 @@ import mongoose from 'mongoose';
 const driverSchema = new mongoose.Schema({
   name: { type: String, required: true },
   contactNumber: { type: String, required: true },
-  vehicleNumber: { type: String, required: true }
+  vehicleNumber: { type: String, required: true },
+  availableDays: { type: [String], required: true }, // Array of weekdays (e.g., ['Monday', 'Wednesday', 'Friday'])
+  availabilityStartTime: { type: String, required: true }, // e.g., "08:00"
+  availabilityEndTime: { type: String, required: true }, // e.g., "18:00"
+  currentAvailability: { type: Boolean, default: false }, // Whether the driver is currently available
+  category: { type: String, required: true },
+  parkName: { type: String, required: true },
+  points: { type: Number, default: 0 } // Driver points
 });
 
 const Driver = mongoose.model('Driver', driverSchema);
